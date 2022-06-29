1ca726b768e4d62c412d260fd39e8b18fb5b349a
Dony Al-Uzzam <46816801+DonyAlUzzam@users.noreply.github.com>
2022-06-29 19:43:35 +0700

    Delete src/config directory

--- src/config/database.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    multipleStatements  : true,
-    host                : '172.17.62.52',
-    user                : 'demo',
-    password            : 'Demo@2020',
-    database            : 'demo'
-};
